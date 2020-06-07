d592f2eeeafd3b3f95367d80685beeaea98783df
Matteo Fiorio <matteo.fiorio@musement.com>
2020-06-07 19:52:48 +0200

    refactor: added extra details to result

--- index.js
+++ index.js
@@ -1,26 +1,72 @@
 const rawData = require('./data.json');
 
+// cache to keep aggragationOverall data to improve getDetails performance
+let cacheAggragationOverall = null;
+
 /**
- * Util to get filtered data 
+ * util to get filtered data based on keyName and keyValue
  * @param {Array} data - array of objects which should contain a key called keyName 
  * @param {String} keyName 
  * @param {String} keyValue 
+ * @returns {Object}
  */
-function getDetails (data, keyName, keyValue) {
-  // using filter to avoid polyfill for browser compatibility
+function getFilteredData (data, keyName, keyValue) {
+  // (using filter to avoid polyfill for browser compatibility)
   // assuming only one keyName is present in data
-  return data.filter(obj => obj[keyName] === keyValue)[0] || {} // default value if no result are found
+  return data.filter(obj => obj[keyName] === keyValue)[0] || {}; // default value if no result are found
 }
 
-// pre-proccess rawData in order to reduce getRangeData complexity
-function getScores () {
-  const aggregationOverall = getDetails(rawData.data, 'slug', 'aggregation-overall'); 
-  return getDetails(aggregationOverall.details, 'key', 'score');
+
+/**
+ * util to pre-process rawData and return an object with required details
+ * @param {String} keyValue 
+ * @returns {Object}
+ */
+function getDetails (keyValue) {
+  if (!cacheAggragationOverall) {
+    cacheAggragationOverall = getFilteredData(rawData.data, 'slug', 'aggregation-overall'); 
+  }
+  return getFilteredData(cacheAggragationOverall.details, 'key', keyValue);
 };
 
-function getRangeData (startDate, endDate) {
-  const scores = getScores();
-  return scores.series.filter(score => score.x >= startDate && score.x <= endDate);
+
+/**
+ * create a dictionary with date as key and info as value
+ * in order to have constat time complexity for search in getRangeData
+ * @returns {Object}
+ */
+function getExtras () {
+  const extras = getDetails('extra');
+
+  return extras.series.reduce((acc, serie) => {
+    return {
+      ...acc,
+      [serie.x]: serie.y
+    }
+  }, {});
 };
+
+
+module.export = function getRangeData (startDate, endDate) {
+  // pre-proccess rawData in order to reduce complexity
+  const scores = getDetails('score');
+  const extras = getExtras();
+
+  return scores.series.reduce((acc, score) => {
+    // assuming dates are in ISO format with no time zone 
+    if (score.x >= startDate && score.x <= endDate) {
+      return [
+        ...acc,
+        {
+          ...score,
+          extra: extras[score.x]
+        }
+      ]
+    }
+    return acc;
+  }, []);
+};
+
+
 
 
