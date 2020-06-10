1881ae6eb2b2066c6b4e1e7816e7144a73bb0578
Matteo Fiorio <matteo.fiorio@musement.com>
2020-06-10 03:03:11 +0200

    refactor: add date parsing, date validity check, abstract some constants, add util to include start|end dates

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const rawData = require('./data.json');
 
-// cache to keep aggragationOverall data to improve getDetails performance
-let cacheAggragationOverall = null;
+const AGGREGATION_SLUG = 'aggregation-overall';
+const AGGREGATION_DETAILS_KEY = 'details'
+const ADDITIONAL_INFO_KEY = 'extra'
+const DETAILS_SERIES_KEY = 'score'
+const DEFAULT_Y_VALUE = 0
+
+// cache to keep aggragationOverall data
+let cacheAggregation = null;
+
 
 /**
  * util to get filtered data based on keyName and keyValue
@@ -10,8 +17,7 @@ let cacheAggragationOverall = null;
  * @param {String} keyValue 
  * @returns {Object}
  */
-function getFilteredData (data, keyName, keyValue) {
-  // (using filter to avoid polyfill for browser compatibility)
+function getFilteredData (data, keyName, keyValue) 
   // assuming only one keyName is present in data
   return data.filter(obj => obj[keyName] === keyValue)[0] || {}; // default value if no result are found
 }
@@ -23,10 +29,10 @@ function getFilteredData (data, keyName, keyValue) {
  * @returns {Object}
  */
 function getDetails (keyValue) {
-  if (!cacheAggragationOverall) {
-    cacheAggragationOverall = getFilteredData(rawData.data, 'slug', 'aggregation-overall'); 
+  if (!cacheAggregation) {
+    cacheAggregation = getFilteredData(rawData.data, 'slug', AGGREGATION_SLUG); 
   }
-  return getFilteredData(cacheAggragationOverall.details, 'key', keyValue);
+  return getFilteredData(cacheAggregation[AGGREGATION_DETAILS_KEY], 'key', keyValue);
 };
 
 
@@ -35,38 +41,83 @@ function getDetails (keyValue) {
  * in order to have constat time complexity for search in getRangeData
  * @returns {Object}
  */
-function getExtras () {
-  const extras = getDetails('extra');
+function getAdditionalInfo () {
+  const { series } = getDetails(ADDITIONAL_INFO_KEY);
 
-  return extras.series.reduce((acc, serie) => {
+  return series.reduce((acc, {x, y}) => {
     return {
       ...acc,
-      [serie.x]: serie.y
+      [x]: y || {} // default value if no result are found
     }
   }, {});
 };
 
 
-module.export = function getRangeData (startDate, endDate) {
-  // pre-proccess rawData in order to reduce complexity
-  const scores = getDetails('score');
-  const extras = getExtras();
+/**
+ * add startDate and endDate if not present in data,
+ * assuming y's default value for line-graph is 0
+ * @param {Array} data 
+ * @param {String} startDate 
+ * @param {String} endDate 
+ */
+function getWholeRange (data, startDate, endDate) {
+  let partial = data;
 
-  return scores.series.reduce((acc, score) => {
-    // assuming dates are in ISO format with no time zone 
-    if (score.x >= startDate && score.x <= endDate) {
-      return [
-        ...acc,
-        {
-          ...score,
-          extra: extras[score.x]
-        }
-      ]
-    }
-    return acc;
-  }, []);
+  if (partial[0].x !== startDate) {
+    partial.unshift({
+      y: DEFAULT_Y_VALUE,
+      x: startDate
+    })
+  };
+
+  if (partial[partial.length - 1].x !== endDate) {
+    partial.push({
+      y: DEFAULT_Y_VALUE,
+      x: endDate
+    })
+  };
+
+  return partial;
 };
 
 
+function getScoresSubset (startDate, endDate) {
+  // parse input params to get Unix time
+  const paresdStartDate = Date.parse(startDate);
+  const paresdEndDate = Date.parse(endDate);
 
+  // check input params validity
+  if (!paresdStartDate || !paresdEndDate) {
+    console.error('[ Error getRangeData ]: input params are not properly formatted')
+    return []
+  }
 
+  // pre-proccess rawData in order to reduce time complexity
+  const { series } = getDetails(DETAILS_SERIES_KEY);
+  const extras = getAdditionalInfo();
+
+  // assuming series are not sorted by x 
+  let partialResult = series.filter((score, idx) => {
+
+    const parsedX = Date.parse(score.x);
+
+    if (!parsedX) {
+      console.warn(`[ Error getRangeData ]: score date at index ${idx} is not properly formatted`);
+      return false;
+    }
+
+    return parsedX >= paresdStartDate && parsedX <= paresdEndDate
+  });
+
+  // add startDate and endDate if they didn't match any scores
+  partialResult = getWholeRange(partialResult, startDate, endDate);
+
+  // add extra info to scores
+  return partialResult.reduce((acc, score) => ([
+    ...acc,
+    {
+      ...score,
+      extra: extras[score.x] || {}
+    }
+  ]), []);
+};
